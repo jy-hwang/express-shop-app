9b413a0684e63fb1923e9b47e64a2108a7212f42
jy-hwang <stratojy@gmail.com>
2024-08-21 00:38:30 +0900

    feat. 상품 등록 페이지
    1. 상품 등록 페이지 router 추가
    2. bootstrap 이용한 페이지 꾸며주기

--- src/routes/admin-products.router.js
+++ src/routes/admin-products.router.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const { checkAdmin } = require('../middleware/auth');
+const Category = require('../models/categories.model');
 
 const router = express.Router();
+
+router.get('/add-product', checkAdmin, async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    res.render('admin/add-product', { categories });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 module.exports = router;
