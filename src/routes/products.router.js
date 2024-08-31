d287f18a737a6726b51226bc40c1bc63fadc0078
jy-hwang <stratojy@gmail.com>
2024-08-31 19:01:51 +0900

    feat. 메인페이지 생성
    1. 로그인 후 이동하는 메인 페이지 /products 의 내용 구성하기
    2. 해당하는 router 내용 수정
    3. 기존에는 페이지 render 하는 부분만 있었음.
    4. 'category' 전체 내용을 조회해서 가져오는 middleware 를 하나 추가하여, 리다이렉트 시 호출되도록 수정.

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
+const { getAllCategories } = require('../middleware/product');
+const Product = require('../models/products.model');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('products');
+router.get('/', getAllCategories, async function (req, res) {
+  try {
+    const products = await Product.find();
+    res.render('products', {
+      products,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
