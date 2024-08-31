8698c6b19126aedf69960753e89da27b1fc5c1aa
jy-hwang <stratojy@gmail.com>
2024-08-31 19:52:29 +0900

    feat. 카테고리 페이지 생성
    1. 메인 페이지에서 각 카테고리를 클릭했을 때 넘어오는 라우터를 설정함.
    2. 카테고리 별로 등록된 상품을 검색하여 화면에 보여줌.
    3. 이전에 등록했던 getAllCategories 미들웨어를 사용함.

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -3,11 +3,25 @@ const { getAllCategories } = require('../middleware/product');
 const Product = require('../models/products.model');
 const router = express.Router();
 
-router.get('/', getAllCategories, async function (req, res) {
+router.get('/', getAllCategories, async (req, res, next) => {
   try {
     const products = await Product.find();
     res.render('products', {
       products,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/:category', getAllCategories, async (req, res, next) => {
+  const categorySlug = req.params.category;
+
+  try {
+    const products = await Product.find({ category: categorySlug });
+    res.render('products', {
+      products: products,
     });
   } catch (error) {
     console.error(error);
